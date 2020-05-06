0eba27ce92cec9f2ab8eb31c8b7569ea62d88ac2
Erik Åström <astromerik93@gmail.com>
2020-05-06 19:39:38 +0000

    Added functionality to display courses and information on course.html. Added placeholder for search functionality

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
 
 
 def courses(request):
-    """ A view to render index.html (Home page)"""
-    return render(request, 'courses/courses.html')
+    """ A view to render all courses available """
+
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'courses/courses.html', context)
