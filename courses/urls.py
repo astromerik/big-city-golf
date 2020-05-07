43f524c2d86545858e44a7faa618afa928adfc1e
Erik Åström <astromerik93@gmail.com>
2020-05-07 11:19:32 +0000

    Added course detail template, detail path and added core frontend to course_detail.html

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.courses, name='courses'),
+    path('<int:course_id>/', views.course_detail, name='course_detail')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 # Create your views here.
 
@@ -13,3 +13,14 @@ def courses(request):
     }
 
     return render(request, 'courses/courses.html', context)
+
+def course_detail(request, course_id):
+    """ A view to show information about a specific golf course """
+    
+    course = get_object_or_404(Course, pk=course_id)
+
+    context = {
+        'course': course,
+    }
+
+    return render(request, 'courses/course_detail.html', context)
