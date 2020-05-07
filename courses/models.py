d21bce126a60bb0d27ba6762b0968560b1c71a2a
Erik Åström <astromerik93@gmail.com>
2020-05-07 11:41:33 +0000

    Installed django-phone-number to store phone numbers in model. Updated model with description, phone number and email to clubs

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
@@ -16,11 +17,14 @@ class District(models.Model):
 class Course(models.Model):
     district = models.ForeignKey('District', null=True, blank=True, on_delete=models.SET_NULL)
     course_name = models.CharField(max_length=50)
+    course_description = models.TextField(null=True, blank=True)
     holes = models.IntegerField(null=True, blank=True)
     green_fee = models.IntegerField(null=True, blank=True)
     rating = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     facility_url = models.URLField(max_length=1024, null=True, blank=True)
     img_url = models.URLField(max_length=1024, null=True, blank=True)
+    facility_phone = PhoneNumberField(null=True, blank=True)
+    facility_email = models.EmailField(null=True, blank=True)
 
     def __str__(self):
         return self.course_name
