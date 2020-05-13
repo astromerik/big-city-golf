d9e63bc44da63cabf158c2715c83d26035184572
Erik Åström <astromerik93@gmail.com>
2020-05-13 13:55:42 +0000

    added sorting functionality to course page

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Course
+from .models import Course, District
 # Create your views here.
 
 
@@ -9,25 +9,46 @@ def courses(request):
     """ A view to render all courses available and search/filter """
 
     courses = Course.objects.all()
-    query = None 
-    
+    query = None
+    districts = None
+    direction = None
+    sort = None
+
     if request.GET:
-        if 'a' in request.GET:
-            query = request.GET['a']
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            courses = courses.order_by(sortkey)
+
+        if 'district' in request.GET:
+            district = request.GET['district']
+            courses = courses.filter(district__name=district)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
             if not query:
                 messages.error(request, "No search criteria used")
                 return redirect(reverse('courses'))
 
             queries = Q(course_name__icontains=query)
             courses = courses.filter(queries)
-    
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'courses': courses,
-        'search_term': query
+        'search_term': query,
+        'current_districts': districts,
+        'current sorting': current_sorting,
     }
 
     return render(request, 'courses/courses.html', context)
-    
+
 
 def course_detail(request, course_id):
     """ A view to show information about a specific golf course """
@@ -41,4 +62,9 @@ def course_detail(request, course_id):
     return render(request, 'courses/course_detail.html', context)
 
 
+def book_tee_time(request, course_id):
+    """ A view to book a tee time """
 
+    course = get_object_or_404(Course, pk=course_id)
+
+    
