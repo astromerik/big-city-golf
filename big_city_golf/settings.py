38fe1d68c095affe825ed021ff16af6e309533ce
Erik Åström <astromerik93@gmail.com>
2020-05-15 08:51:09 +0000

    Added database and migrated prior push to heroku

--- big_city_golf/settings.py
+++ big_city_golf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'uz36g27qi&*=6#c=&3!)54agdx_1w73#7nhaws23p8p9(1@f#('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'courses',
     'golfprofile',
     'phonenumber_field',
+
 ]
 
 MIDDLEWARE = [
@@ -105,13 +107,17 @@ WSGI_APPLICATION = 'big_city_golf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# if 'DATABASE_URL' in os.environ:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
