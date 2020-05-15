d898c1775463cd8c747be232b86ba44b4e94bd5e
Erik Åström <astromerik93@gmail.com>
2020-05-15 09:22:19 +0000

    Added allowed local host and set database for local and heroku usage

--- big_city_golf/settings.py
+++ big_city_golf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'uz36g27qi&*=6#c=&3!)54agdx_1w73#7nhaws23p8p9(1@f#('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'astromerik-big-city-golf.herokuapp.com']
 
 
 # Application definition
@@ -107,17 +107,17 @@ WSGI_APPLICATION = 'big_city_golf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
