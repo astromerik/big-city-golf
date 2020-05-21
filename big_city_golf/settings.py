603acb816ecc96deb8415eb7325cbadb1f0c7e32
Erik Åström <astromerik93@gmail.com>
2020-05-21 15:22:48 +0000

    changed direction for STATIC_ROOT

--- big_city_golf/settings.py
+++ big_city_golf/settings.py
@@ -171,6 +171,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'site/assets')
 
 # LOCAL
 # MEDIA_URL = '/media/'
