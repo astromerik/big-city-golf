930595bf6cae2eed6fd52e59c073fd14541f10ce
Erik Åström <astromerik93@gmail.com>
2020-05-21 15:08:12 +0000

    changed static files settings in settings.py to test heroku deployment

--- big_city_golf/settings.py
+++ big_city_golf/settings.py
@@ -171,8 +171,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 # LOCAL
 # MEDIA_URL = '/media/'
