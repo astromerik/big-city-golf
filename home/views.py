719cffb1b7511cdb3c38dd65bcf241d59e4ccabf
Erik Åström <astromerik93@gmail.com>
2020-05-21 13:47:58 +0000

    added a simple about page with pictures

--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def index(request):
     """ A view to render index.html (Home page)"""
     return render(request, 'home/index.html')
+
+def about(request):
+    """ A view to render about.html (About page)"""
+    return render(request, 'home/about.html')
